a39375ebe74bc1d683cd183530add42fd2ffb390
cpascal-gr <cpascal.gr@gmail.com>
2018-09-27 19:33:14 +0200

    Add hasAutostartPermissionPopup() function

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 module.exports = {
     openAutostartPermissionPopup: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "AutostartPermission", "openAutostartPermissionPopup");
+    },
+    hasAutostartPermissionPopup: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "AutostartPermission", "hasAutostartPermissionPopup");
     }
 };
